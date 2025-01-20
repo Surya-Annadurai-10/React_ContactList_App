0cdf2f8f3e392e705d29f60ca703ecc95168a3d1
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-01-21 00:26:12 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,43 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Login from './Containers/Login/Login'
+import Layout from './Containers/Layout/Layout'
+import BlogList from './Containers/BlogList/BlogList'
+import BlogCreate from './Containers/BlogCreate/BlogCreate'
+import { createContext } from 'react'
+
+export const UserContext = createContext();
 
 function App() {
-  const [count, setCount] = useState(0)
+const [userDetails , setUserDetails] = useState({});
+
+const router = createBrowserRouter ([
+  {
+    path : '/',
+    element : <Login />
+  },
+  {
+    element : <Layout />,
+    children : [
+      {
+        path : "/home",
+        element : <BlogList />
+      },
+      {
+        path : "/create",
+        element : <BlogCreate />
+      }
+    ]
+  }
+])
 
   return (
    <>
-    <h1>Contact List App</h1>
+   <UserContext.Provider value={{userDetails,setUserDetails}}>
+      <RouterProvider router = {router} />
+   </UserContext.Provider>
    </>
   )
 }
---
+++ src/Components/BlogCard/BlogCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './BlogCard.module.css'
+
+const BlogCard = () => {
+  return (
+    <div>BlogCard</div>
+  )
+}
+
+export default BlogCard
